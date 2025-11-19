24498a62f0d5b2f02969683e69718eff034053c2
nishant5367 <someone@gmail.com>
2025-11-19 15:54:23 +0530

    Initial files

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_gamify';   // Full frankenstyle name.
+$plugin->version   = 2025091701;       // YYYYMMDDNN (odd numbers for Nishant).
+$plugin->requires  = 2022041900;       // Requires Moodle 4.0.1.
+$plugin->maturity  = MATURITY_ALPHA;   // Development stage.
+$plugin->release   = '0.0.1-dev';
