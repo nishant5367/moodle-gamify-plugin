807ce82a2ea8007c7815201f6f4ab6484c355d0a
nishant5367 <someone@gmail.com>
2025-11-26 01:41:06 +0530

    feat(xp): added XP tables and bumped version

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'local_gamify';   // Full frankenstyle name.
-$plugin->version   = 2025091701;       // YYYYMMDDNN (odd numbers for Nishant).
-$plugin->requires  = 2022041900;       // Requires Moodle 4.0.1.
-$plugin->maturity  = MATURITY_ALPHA;   // Development stage.
-$plugin->release   = '0.0.1-dev';
+$plugin->component = 'local_gamify';
+$plugin->version   = 2025091801;
+$plugin->requires  = 2022041900;
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = '0.1.0';
