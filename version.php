56fa86cc79df208c17a612e6a63f0024bf6be021
nishant5367 <someone@gmail.com>
2025-11-26 03:09:46 +0530

    feat(xp): finalized DB schema (XP + XP events tables)

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_gamify';
-$plugin->version   = 2025091801;
+$plugin->version   = 2025091805;
 $plugin->requires  = 2022041900;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1.0';
